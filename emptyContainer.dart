728d6869a9c4d872024951cb4ab82a0d701f5951
Ciro Zullo <95353984+EncryptedEasty@users.noreply.github.com>
2023-04-09 23:08:06 +0200

    Sync - Added the actual snippets

---
+++ emptyContainer.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/widgets.dart';
+
+Widget getEmptyContainer() {
+  return const SizedBox(width: 0.0, height: 0.0);
+}
---
+++ http.dart
@@ -0,0 +1,72 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:io';
+
+Future<T> get<T>(String url) async {
+  if (url.trim().isNotEmpty) {
+    try {
+      http.Response response = await http.get(Uri.parse(url));
+
+      if (response != null) {
+        if (response.statusCode >= 200 && response.statusCode < 300) {
+          return (T as dynamic).fromJSON(jsonDecode(response.body));
+        } else {
+          dynamic body =
+              response.body != null ? jsonDecode(response.body) : null;
+
+          if (body != null) {
+            throw HttpException(body.message);
+          } else {
+            throw HttpException('test');
+          }
+        }
+      } else {
+        throw HttpException('test');
+      }
+    } on SocketException catch (error) {
+      throw HttpException(error.toString());
+    } on HttpException catch (errorMessage) {
+      print(errorMessage);
+    }
+  } else {
+    throw Exception('URL is empty');
+  }
+}
+
+
+Future<T> post<T>(String url) async {
+  if (url.trim().isNotEmpty) {
+    return (T as dynamic)
+        .fromJson(jsonDecode((await http.post(Uri.parse(url))).body));
+  } else {
+    throw Exception('URL is empty');
+  }
+}
+
+Future<T> patch<T>(String url) async {
+  if (url.trim().isNotEmpty) {
+    return (T as dynamic)
+        .fromJson(jsonDecode((await http.patch(Uri.parse(url))).body));
+  } else {
+    throw Exception('URL is empty');
+  }
+}
+
+Future<T> put<T>(String url) async {
+  if (url.trim().isNotEmpty) {
+    return (T as dynamic)
+        .fromJson(jsonDecode((await http.put(Uri.parse(url))).body));
+  } else {
+    throw Exception('URL is empty');
+  }
+}
+
+Future<T> delete<T>(String url) async {
+  if (url.trim().isNotEmpty) {
+    return (T as dynamic)
+        .fromJson(jsonDecode((await http.delete(Uri.parse(url))).body));
+  } else {
+    throw Exception('URL is empty');
+  }
+}
+
