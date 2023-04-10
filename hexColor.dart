8e03adaf00da0b10a0f2e0f709b9198d255d3e50
Ciro Zullo <95353984+EncryptedEasty@users.noreply.github.com>
2023-04-10 19:09:14 +0200

    Minor improvement to HexColor extension

--- hexColor.dart
+++ hexColor.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 extension HexColor on Color {
-  static Color fromHex(String hexString) {
+  static Color fromHexString(String hexString) {
     final buffer = StringBuffer();
     if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
     buffer.write(hexString.replaceFirst('#', ''));
@@ -9,9 +9,12 @@ extension HexColor on Color {
   }
 
   /// Prefixes a hash sign if [leadingHashSign] is set to `true` (default is `true`).
-  String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
+  String toHexString({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
       '${alpha.toRadixString(16).padLeft(2, '0')}'
       '${red.toRadixString(16).padLeft(2, '0')}'
       '${green.toRadixString(16).padLeft(2, '0')}'
       '${blue.toRadixString(16).padLeft(2, '0')}';
+
+  /// Returns true if this color is opaque (alpha == 255).
+  bool get isOpaque => alpha == 255;
 }
